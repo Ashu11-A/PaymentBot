eabce0061fac02380a280ce9048714ab078a8882
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2023-09-23 17:11:53 -0400

    Melhor clareza

--- src/discord/commands/payments/utils/collector/collectorButtons.ts
+++ src/discord/commands/payments/utils/collector/collectorButtons.ts
@@ -2,86 +2,48 @@ import { db } from '@/app'
 import { ActionRowBuilder, type ButtonInteraction, type CacheType, ModalBuilder, TextInputBuilder } from 'discord.js'
 import { PaymentFunction } from './functions/collectorFunctions'
 
+type CustomIdHandlers = Record<string, () => Promise<void> | void>
+
 export default async function collectorButtons (interaction: ButtonInteraction<CacheType>, key: string, value: any): Promise<void> {
   const { guildId, user, customId } = interaction
   const { title, label, placeholder, style, type, maxLength } = value
-  if (customId === key) {
-    // typeRedeem
-    if (customId === 'paymentUserAdd') {
-      await PaymentFunction.AddOrRem({
-        interaction,
-        type: 'Add'
-      })
-      return
-    }
-    if (customId === 'paymentUserRem') {
-      await PaymentFunction.AddOrRem({
-        interaction,
-        type: 'Rem'
-      })
-      return
-    }
-    if (customId === 'paymentUserDM') {
-      await PaymentFunction.paymentUserDM({
-        interaction
-      })
-      return
-    }
-    if (customId === 'paymentUserNext') {
-      await PaymentFunction.NextOrBefore({
-        interaction,
-        type: 'next'
-      })
-      return
-    }
-    if (customId === 'paymentUserBefore') {
-      await PaymentFunction.NextOrBefore({
-        interaction,
-        type: 'before'
-      })
-      return
-    }
-    if (customId === 'paymentUserGerarPix') {
-      console.log('oi')
-      return
-    }
-    if (customId === 'paymentUserGerarCardDebito') {
-      console.log('oi')
-      return
-    }
-    if (customId === 'paymentUserGerarCardCredito') {
-      console.log('oi')
-      return
-    }
-    if (customId === 'paymentUserWTF') {
-      await PaymentFunction.paymentUserWTF({
-        interaction
-      })
-      return
-    }
-    if (customId === 'paymentUserCancelar') {
-      await PaymentFunction.paymentUserCancelar({
-        interaction
-      })
-      return
-    }
 
-    const textValue = await db.payments.get(`${guildId}.process.${user.id}.${type}`)
-    const modal = new ModalBuilder({ customId: key, title })
-    const content = new ActionRowBuilder<TextInputBuilder>({
-      components: [
-        new TextInputBuilder({
-          custom_id: 'content',
-          label,
-          placeholder,
-          value: textValue ?? null,
-          style,
-          required: true,
-          maxLength
-        })
-      ]
-    })
-    modal.setComponents(content)
-    await interaction.showModal(modal)
+  const customIdHandlers: CustomIdHandlers = {
+    paymentUserAdd: async () => { await PaymentFunction.AddOrRem({ interaction, type: 'Add' }) },
+    paymentUserRem: async () => { await PaymentFunction.AddOrRem({ interaction, type: 'Rem' }) },
+    paymentUserDM: async () => { await PaymentFunction.paymentUserDM({ interaction }) },
+    paymentUserNext: async () => { await PaymentFunction.NextOrBefore({ interaction, type: 'next' }) },
+    paymentUserBefore: async () => { await PaymentFunction.NextOrBefore({ interaction, type: 'before' }) },
+    paymentUserGerarPix: () => { console.log('oi') },
+    paymentUserGerarCardDebito: () => { console.log('oi') },
+    paymentUserGerarCardCredito: () => { console.log('oi') },
+    paymentUserWTF: async () => { await PaymentFunction.paymentUserWTF({ interaction }) },
+    paymentUserCancelar: async () => { await PaymentFunction.paymentUserCancelar({ interaction }) }
+  }
+
+  if (customId === key) {
+    const customIdHandler = customIdHandlers[customId]
+
+    if (typeof customIdHandler === 'function') {
+      await customIdHandler() // Now you can call the function
+    } else {
+      const textValue = await db.payments.get(`${guildId}.process.${user.id}.${type}`)
+      const modal = new ModalBuilder({ customId: key, title })
+      const content = new ActionRowBuilder<TextInputBuilder>({
+        components: [
+          new TextInputBuilder({
+            custom_id: 'content',
+            label,
+            placeholder,
+            value: textValue ?? null,
+            style,
+            required: true,
+            maxLength
+          })
+        ]
+      })
+      modal.setComponents(content)
+      await interaction.showModal(modal)
+    }
   }
 }
