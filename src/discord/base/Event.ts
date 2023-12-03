cea9d725e65b9aa2f2a6f00bf9fae3e762d49f2f
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-12-03 16:48:13 -0400

    🧱 build: fix build run

--- src/discord/base/Event.ts
+++ src/discord/base/Event.ts
@@ -5,7 +5,8 @@ import { type ClientEvents } from 'discord.js'
 interface EventData<Key extends keyof ClientEvents> {
   name: Key
   once?: boolean
-  run: (...args: ClientEvents[Key]) => any
+  // eslint-disable-next-line @typescript-eslint/method-signature-style
+  run(...args: ClientEvents[Key]): any
 }
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
