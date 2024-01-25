5ab67e69a8e75c1380fab40ae10d0227d30e4e7f
Ashu11-A <Matheusn.biolowons@gmail.com>
2024-01-25 03:03:50 -0400

    ❌ Disable Telegram

--- src/discord/events/ready/index.ts
+++ src/discord/events/ready/index.ts
@@ -2,14 +2,14 @@ import { setIntervalAsync } from 'set-interval-async/fixed'
 import { Event } from '@/discord/base'
 import statusPresence from './statusPresence'
 import moduleExpress from '@/express/express'
-import { telegramNotify } from './telegram'
+// import { telegramNotify } from './telegram'
 
 export default new Event({
   name: 'ready',
   async run () {
     await statusPresence()
     await moduleExpress()
-    await telegramNotify()
+    // await telegramNotify()
     setIntervalAsync(async () => {
       await statusPresence()
     }, 15000)
